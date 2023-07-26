9a32641a92f4be8da9b004960f57aebed19e53fb
Anton Lazutin <a.lazutin@malanka.eu>
2023-07-26 16:13:51 +0300

    [IMP] MEL-40 changed source document field to the following format 'Delivery for {sale order name} from {sale order date}'

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Stock picking source',
+    'version': '16.0.0.1',
+    'depends': ['stock'],
+    'author': 'Malanka',
+    'website': 'https://malanka.eu',
+    'category': 'Category',
+    'data': [
+    ],
+}
